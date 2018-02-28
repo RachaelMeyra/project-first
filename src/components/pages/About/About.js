f88a3264506a95e9e23f249b3a4430ca190f82b4
Fatos Meyra Kilic <fatosmeyrakilic@gmail.com>
2018-02-28 17:07:32 +1100

    starting point

---
+++ src/components/pages/About/About.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import './style.scss';
+
+export default class About extends Component {
+
+  render() {
+    return (
+      <div className="about">
+        <h1>
+          About
+        </h1>
+      </div>
+    );
+  }
+}
---
+++ src/components/pages/Resume/Resume.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './style.scss';
+
+class Resume extends Component {
+  render() {
+    return (
+      <div style={{marginTop: '50px'}}>Hello world</div>
+    )
+  }
+}
+
+export default Resume;
